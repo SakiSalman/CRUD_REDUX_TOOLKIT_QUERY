9f69e0d063f5a4983f94aba62af31fa038f53bcb
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-06-18 21:41:07 +0600

    Completed Crud backend

--- crud_to_backend/controllers/todoControllers.js
+++ crud_to_backend/controllers/todoControllers.js
@@ -1,16 +1,85 @@
 const Todo = require("../models/todoModel.js");
 
 
-
+/**
+ * 
+ * @param {GET} req 
+ * @param {JSON} res 
+ * @param {*} next 
+ * @returns TODO
+ */
 const getTodo = async (req, res, next) => {
 
     try {
             const todos = await Todo.find()
+           return res.send(todos)
+    } catch (error) {
+        console.log(error);
+    }
+}
+/**
+ * 
+ * @param {GET} req 
+ * @param {JSON} res 
+ * @param {*} next 
+ * @returns TODO
+ */
+const createTodo = async (req, res, next) => {
 
-            res.send(todos)
+    try {
+        const data = req.body
+        console.log(data);
+            const todos = await Todo.create(data)
+           return res.send(todos)
+    } catch (error) {
+        console.log(error);
+    }
+}
+/**
+ * 
+ * @param {GET} req 
+ * @param {JSON} res 
+ * @param {*} next 
+ * @returns TODO
+ */
+const deleteTodo = async (req, res, next) => {
+
+    try {
+        const {id} = req.params
+            const todos = await Todo.findByIdAndDelete(id)
+           return res.send(todos)
+    } catch (error) {
+        console.log(error);
+    }
+}
+const updateTodo = async (req, res, next) => {
+
+    try {
+        const {id} = req.params
+            const todos = await Todo.findByIdAndUpdate(id, {
+                status : true
+            })
+           return res.send(todos)
+    } catch (error) {
+        console.log(error);
+    }
+}
+/**
+ * 
+ * @param {GET} req 
+ * @param {JSON} res 
+ * @param {*} next 
+ * @returns TODO
+ */
+const getSingelTODO = async (req, res, next) => {
+
+    try {
+        const {id} = req.params
+            const todos = await Todo.findById(id)
+           return res.send(todos)
     } catch (error) {
         console.log(error);
     }
 }
 
-module.exports = {getTodo}
+module.exports = {getTodo, updateTodo, createTodo,deleteTodo, getSingelTODO}
--- crud_to_backend/routes/todo.js
+++ crud_to_backend/routes/todo.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getTodo } = require('../controllers/todoControllers.js')
+const { getTodo, getSingelTODO, deleteTodo, createTodo,updateTodo } = require('../controllers/todoControllers.js')
 
 
 const router = express.Router()
@@ -7,7 +7,11 @@ const router = express.Router()
 
 
 // Get Routes
+router.patch('/todo/:id', updateTodo)
 router.get('/todo', getTodo)
+router.get('/todo/:id', getSingelTODO)
+router.delete('/todo/:id', deleteTodo)
+router.post('/todo', createTodo)
 
 
 // export router
